58b6e549d30a0a7eaf4cf103f536bdb961504a64
quinnc <mrstrunks@ntlworld.com>
2021-05-21 20:58:03 +0100

    Step 3 - Adding a breed - builder pattern

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -5,10 +5,16 @@ import java.time.LocalDateTime;
 public class Dog {
     private final String name;
     private final LocalDateTime dateOfBirth;
+    private final String breed;
 
-    public Dog(String name, LocalDateTime dateOfBirth) {
+    public Dog(String name, LocalDateTime dateOfBirth, String breed) {
         this.name = name;
         this.dateOfBirth = dateOfBirth;
+        this.breed = breed;
+    }
+
+    public static DogBreeder called(String name) {
+        return new DogBreeder(name);
     }
 
     public String getName() {
@@ -17,5 +23,30 @@ public class Dog {
 
     public LocalDateTime getDateOfBirth() {
         return dateOfBirth;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public static class DogBreeder {
+        private final String name;
+        private String breed;
+        private LocalDateTime dateOfBirth;
+
+        public DogBreeder(String name) {
+
+            this.name = name;
+        }
+
+        public DogBreeder ofBreed(String breed) {
+            this.breed = breed;
+            return this;
+        }
+
+        public Dog bornOn(LocalDateTime dateOfBirth) {
+            this.dateOfBirth = dateOfBirth;
+            return new Dog(name, dateOfBirth, breed);
+        }
     }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenWeCreateANewDog.java
@@ -8,11 +8,12 @@ import java.time.LocalDateTime;
 public class WhenWeCreateANewDog {
     @Test
     public void a_new_dog_should_have_a_name() {
-        LocalDateTime birthday = LocalDateTime.now();
-        Dog jasper = new Dog("Jasper", birthday);
+        LocalDateTime THE_FOURTH_OF_JULY = LocalDateTime.of(2021,7,4,0,0);
+        Dog jasper = Dog.called("Jasper").ofBreed("Poodle").bornOn(THE_FOURTH_OF_JULY);
 
         Assert.assertEquals("Jasper", jasper.getName());
-        Assert.assertEquals(birthday, jasper.getDateOfBirth());
+        Assert.assertEquals(THE_FOURTH_OF_JULY, jasper.getDateOfBirth());
+        Assert.assertEquals("Poodle", jasper.getBreed());
 
     }
 }
